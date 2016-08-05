40e19a4b02732e2370427f7de0055a0a8e594eed
ghKinG <nimaghorbani@users.noreply.github.com>
2016-08-05 04:41:18 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,30 +226,17 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {181003660},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
+    about_text = [[FakeBot v1 
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
-
-Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
+nimagh(@nimagh315)
 
 Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+English: @FakeEn
+Persian: @FakePer
 ]],
     help_text_realm = [[
 Realm Commands:
